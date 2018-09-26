fc768e37f868e033e6be1cc727dc3683c1b00362
Andrej Staruch <staruch.andrej@gmail.com>
2018-09-26 18:52:44 +0200

    Skeleton of module

---
+++ 02-classes/scorelib.py
@@ -0,0 +1,30 @@
+from sys import argv
+
+
+class Print:
+    pass
+
+
+class Edition:
+    pass
+
+
+class Composition:
+    pass
+
+
+class Voice:
+    pass
+
+
+class Person:
+    pass
+
+
+def main():
+    print(argv)
+    pass
+
+
+if __name__ == 'main':
+    main()
