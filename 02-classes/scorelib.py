7b6e5a0686c902facafdb1b963b7ecc2d66edfb0
Staruch, Andrej <andrej.staruch@solarwinds.com>
2018-09-29 15:33:39 +0200

    Constructors.

--- 02-classes/scorelib.py
+++ 02-classes/scorelib.py
@@ -2,23 +2,57 @@ from sys import argv
 
 
 class Print:
-    pass
+    def __init__(self, print_id, edition, partiture):
+        self.edition = edition
+        self.print_id = print_id
+        self.partiture = partiture
+
+    def format(self):
+        print('Print Number: {}'.format(self.print_id), )
+
+    def composition(self):
+        return self.edition.composition
 
 
 class Edition:
-    pass
+    def __init__(self, composition, name):
+        self.composition = composition
+        self.authors = []
+        self.name = name
+
+    def add_author(self, name):
+        self.authors.append(Person(name))
 
 
 class Composition:
-    pass
+    def __init__(self, name, incipit, key, genre, year, composer):
+        self.name = name
+        self.incipit = incipit
+        self.key = key
+        self.genre = genre
+        self.year = year
+        self.composer = Person(composer)
+        self.voices = []
+        self.authors = []
+
+    def add_voice(self, voice_range, name):
+        self.voices.append(Voice(voice_range, name))
+
+    def add_author(self, name):
+        self.authors.append(Person(name))
 
 
 class Voice:
-    pass
+    def __init__(self, voice_range, name):
+        self.range = voice_range
+        self.name = name
 
 
 class Person:
-    pass
+    def __init__(self, name, born, died):
+        self.name = name
+        self.born = born
+        self.died = died
 
 
 def main():
@@ -26,5 +60,5 @@ def main():
     pass
 
 
-if __name__ == 'main':
+if __name__ == '__main__':
     main()
