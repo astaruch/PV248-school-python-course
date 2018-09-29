7b9d6a6d4706834d00dc389ac4dc6429d698b226
Staruch, Andrej <andrej.staruch@solarwinds.com>
2018-09-29 20:41:51 +0200

    Creating composition

--- 02-classes/scorelib.py
+++ 02-classes/scorelib.py
@@ -9,7 +9,8 @@ class Print:
         self.partiture = partiture
 
     def format(self):
-        print('Print Number: {}'.format(self.print_id), )
+        if self.print_id:
+            print("Print Number: {}".format(self.print_id))
 
     def composition(self):
         return self.edition.composition
@@ -21,26 +22,25 @@ class Edition:
         self.authors = []
         self.name = name
 
-    def add_author(self, name):
-        self.authors.append(Person(name))
+    def add_author(self, name, born, died):
+        self.authors.append(Person(name, born, died))
 
 
 class Composition:
-    def __init__(self, name, incipit, key, genre, year, composer):
+    def __init__(self, name, incipit, key, genre, year):
         self.name = name
         self.incipit = incipit
         self.key = key
         self.genre = genre
         self.year = year
-        self.composer = Person(composer)
         self.voices = []
         self.authors = []
 
     def add_voice(self, voice_range, name):
         self.voices.append(Voice(voice_range, name))
 
-    def add_author(self, name):
-        self.authors.append(Person(name))
+    def add_author(self, name, born, died):
+        self.authors.append(Person(name, born, died))
 
 
 class Voice:
@@ -52,38 +52,38 @@ class Voice:
 class Person:
     def __init__(self, name, born, died):
         self.name = name
-        self.born = born
-        self.died = died
+        self.born = None if born == '' else born
+        self.died = None if died == '' else died
 
 
 def load(filename):
     re_print = re.compile(r'Print Number: (\d+)')
     re_composer = re.compile(r'Composer: (.*)')
     re_title = re.compile(r'Title: (.*)')
-    re_genre = re.compile(r'Genre: (.*)')
-    re_key = re.compile(r'Key: (.*)')
-    re_composition_year = re.compile(r'Composition Year: (\d+)')
-    re_publication_year = re.compile(r'Publication Year: (\d+)')
+    re_genre = re.compile(r'Genre: ?(.*)?')
+    re_key = re.compile(r'Key: ?(.*)?')
+    re_composition_year = re.compile(r'Composition Year: ?(\d+)?')
+    re_publication_year = re.compile(r'Publication Year: ?(\d+)?')
     re_edition = re.compile(r'Edition: (.*)')
-    re_editor = re.compile(r'Editor: (.*)')
-    re_voice = re.compile(r'Voice \d+: (.*)')
+    re_editor = re.compile(r'Editor: ?(.*)?')
+    re_voice = re.compile(r'Voice \d+: ?(.*)?')
     re_partiture = re.compile(r'Partiture: (.*)')
-    re_incipit = re.compile(r'Incipit: (.*)')
+    re_incipit = re.compile(r'Incipit ?\d?: ?(.*)?')
     re_new_line = re.compile(r'\n')
 
-    prints = []
+    prints = voice_lines = []
     with open(filename, 'r', encoding='utf8') as f:
         for line in f:
             match = re_print.match(line)
-            if match:  # print number
+            if match:
                 print_id = match.group(1)
                 continue
             match = re_composer.match(line)
-            if match:  # composers names and their years of born/die
+            if match:
                 composer_line = match.group(1)
                 continue
             match = re_title.match(line)
-            if match:  # not used
+            if match:
                 title = match.group(1)
                 continue
             match = re_genre.match(line)
@@ -104,7 +104,7 @@ def load(filename):
                 continue
             match = re_edition.match(line)
             if match:
-                edition = match.group(1)
+                edition_title = match.group(1)
                 continue
             match = re_editor.match(line)
             if match:
@@ -112,25 +112,48 @@ def load(filename):
                 continue
             match = re_voice.match(line)
             if match:
-                voice_line = match.group(1)
+                voice_lines.append(match.group(1))
                 continue
             match = re_partiture.match(line)
             if match:
-                partiture = match.group(1)
+                partiture = True if match.group(1) == 'yes' else False
                 continue
             match = re_incipit.match(line)
             if match:
                 incipit = match.group(1)
                 continue
             match = re_new_line.match(line)
-            if match:
-                # create objects and then clear them
-                print(print_id, composer_line, title, genre, key,
-                      composition_year, publication_year, edition, editor,
-                      voice_line, partiture, incipit)
+            if match and print_id:
+                composition = Composition(title, incipit, key, genre,
+                                          composition_year)
+
+                composer_line.split(';')
+                for composer in composer_line.split(';'):
+                    name = born = died = None
+                    if '--' in composer:
+                        match = re.match(r'(.*) \((\d+)?--(\d+)?.*\)',
+                                         composer)
+                        name = match.group(1)
+                        born = match.group(2)
+                        died = match.group(3)
+                    else:
+                        name = composer
+                    composition.add_author(name, born, died)
+
+                edition = Edition(composition, edition_title)
+                record = Print(print_id, edition, partiture)
+
+                print(record)
+                record.format()
+                prints.append(record)
+
                 print_id = composer_line = title = genre = key = None
-                composition_year = publication_year = edition = editor = None
-                voice_line = partiture = incipit = None
+                composition_year = publication_year = edition_title = None
+                editor = None
+                voice_lines = []
+                partiture = incipit = None
+                continue
+            print('Some unexpected line')
     f.closed
 
     return prints
