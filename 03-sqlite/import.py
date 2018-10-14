b4702cb4004486f22df8ba9d428faec4982991d9
Andrej Staruch <staruch.andrej@gmail.com>
2018-10-14 18:54:47 +0200

    Order of methods.

--- 03-sqlite/import.py
+++ 03-sqlite/import.py
@@ -4,18 +4,12 @@ import os
 import scorelib
 
 
-def insert_person(db_cursor, person):
-    query = "INSERT INTO person(born, died, name) VALUES(?, ?, ?)"
-    values = (person.born, person.died, person.name)
-    db_cursor.execute(query, values)
+def create_tables(db_cursor, input_schema, output_db):
+    with open(input_schema, 'r') as sql_schema:
+        tables = sql_schema.read()
+        db_cursor.executescript(tables)
 
 
-def insert_score(db_cursor, score):
-    query = """
-    INSERT INTO score(name, genre, key, incipt, year) VALUES (?, ?, ?, ?, ?)
-    """
-    values = (score.name, score.genre, score.key, score.incipit, score.year)
-    db_cursor.execute(query, values)
 
 
 def insert_voice(db_cursor, voice, score_id):
@@ -48,18 +42,34 @@ def insert_print(db_cursor, print, edition_id):
     db_cursor.execute(query, values)
 
 
-def create_tables(db_cursor, input_schema, output_db):
-    with open(input_schema, 'r') as sql_schema:
-        tables = sql_schema.read()
-        db_cursor.executescript(tables)
+def insert_score(db_cursor, score):
+    query = """
+    INSERT INTO score(name, genre, key, incipt, year) VALUES (?, ?, ?, ?, ?)
+    """
+    values = (score.name, score.genre, score.key, score.incipit, score.year)
+    db_cursor.execute(query, values)
+    return db_cursor.lastrowid
 
 
-def persist_people(db_cursor, _print):
+def persist_score(db_cursor, score):
+    query = "SELECT COUNT(*) FROM score WHERE score.name = ?"
+    db_cursor.execute(query, (score.name, ))
+
+
+def persist_print(db_cursor, _print):
+    persist_score(db_cursor, _print.composition())
+
     for editor in _print.edition.authors:
         editor_id = persist_person(db_cursor, editor)
 
     for composer in _print.composition().authors:
         composer_id = persist_person(db_cursor, composer)
+
+
+def insert_person(db_cursor, person):
+    query = "INSERT INTO person(born, died, name) VALUES(?, ?, ?)"
+    values = (person.born, person.died, person.name)
+    db_cursor.execute(query, values)
 
 
 def persist_person(db_cursor, person):
@@ -105,7 +115,7 @@ def main():
 
     prints = scorelib.load(input_source)
     for p in prints:
-        persist_people(db_curs, p)
+        persist_print(db_curs, p)
 
     db_conn.commit()
 
