969eee8b0c84bcc6167d7aaa4b2dd875ab556293
Andrej Staruch <staruch.andrej@gmail.com>
2018-11-27 23:06:33 +0100

    Fixes
    Separate var for headers
    Explicit passing request
    Returning handler
    Correct parameters for class method
    Typos

--- 09-http-requests/http-forward.py
+++ 09-http-requests/http-forward.py
@@ -8,12 +8,14 @@ def wrap_handler(url):
     class RequestHandler(http.server.BaseHTTPRequestHandler):
         def do_GET(self):
             target_url = 'http://' + url if url[:4] != 'http' else url
-
+            headers = dict(self.headers)
             request = urllib.request.Request(url=target_url,
-                                             headers=self.headers,
+                                             headers=headers,
                                              data=None)
             try:
-                response = urllib.request.urlopen(request, timeout=1)
+                response = urllib.request.urlopen(url=request,
+                                                  timeout=1)
+                # response = response.read().decode()
                 return self.process_response_from_server(response)
             except urllib.error.URLError:
                 print("Request has timeouted")
@@ -26,7 +28,7 @@ def wrap_handler(url):
 
         def do_POST(self):
             # Kindly borrowed from https://stackoverflow.com/a/5976905/6838293
-            content_len = int(self.headers.getheader('content-length', 0))
+            content_len = int(self.headers.get('content-length', 0))
             post_body = self.rfile.read(content_len)
 
             try:
@@ -43,6 +45,8 @@ def wrap_handler(url):
             url = json_body["url"]
             headers = json_body["headers"] if "headers" in json_body else dict()
             content = json_body["content"] if method == "POST" else None
+            if content:
+                content = json.dumps(content).encode()
             timeout = json_body["timeout"] if "timeout" in json_body else 1
 
             request = urllib.request.Request(url=url,
@@ -50,6 +54,7 @@ def wrap_handler(url):
                                              headers=headers)
             try:
                 response = urllib.request.urlopen(request, timeout=timeout)
+                # content = response.read().decode()
                 content = self.process_response_from_server(response)
                 return self.send_response_to_client(content)
             except urllib.error.URLError:
@@ -61,7 +66,7 @@ def wrap_handler(url):
                 content = json.dumps({"code": e.code})
                 return self.send_response_to_client(content)
 
-        def process_response_from_server(response):
+        def process_response_from_server(self, response):
             response_dict = {}
             response_dict['code'] = response.status
             response_dict['headers'] = dict(response.getheaders())
@@ -74,11 +79,12 @@ def wrap_handler(url):
             return json.dumps(response_dict)
 
         def send_response_to_client(self, content):
-            self.send_reponse(200)
+            self.send_response(200)
             self.send_header('Content-Type', 'application/json')
             self.end_headers()
-            content = json.dumps(content)
+            # content = json.dumps(content)
             self.wfile.write(content.encode())
+    return RequestHandler
 
 
 def main():
@@ -88,8 +94,9 @@ def main():
     port = argv[1]
     upstream = argv[2]
     listening_address = ('localhost', int(port))
+    handler = wrap_handler(upstream)
     httpd = http.server.HTTPServer(server_address=listening_address,
-                                   RequestHandlerClass=wrap_handler(upstream))
+                                   RequestHandlerClass=handler)
     httpd.serve_forever()
 
 
