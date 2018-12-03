2a02ee9681c1231b769c03097711246661ad6993
Andrej Staruch <staruch.andrej@gmail.com>
2018-12-03 21:01:40 +0100

    Fixed serving cgi

--- 10-common-gateway-interface/serve.py
+++ 10-common-gateway-interface/serve.py
@@ -13,11 +13,9 @@ def wrap_handler():
         """ Modify cgi_directories to accepts current directory"""
         def __init__(self, *args, **kwargs):
             super(self.__class__, self).__init__(*args, **kwargs)
-            print('cwd: ', os.getcwd())
-            cwd = os.getcwd().split(os.path.sep)[-1]
-            cgi_dir = '/' + cwd
-            print('Appending {} to "cgi_directories"...'.format(cgi_dir))
-            self.cgi_directories.append(cgi_dir)
+            curr_dir = os.path.dirname(self.path)
+            if curr_dir not in self.cgi_directories:
+                self.cgi_directories.append(curr_dir)
 
         def do_HEAD(self):
             self.do_REQUEST()
@@ -32,10 +30,8 @@ def wrap_handler():
             print("Request = ", self.path)
             """Test whether self.path corresponds to a CGI script."""
             if os.path.splitext(self.path)[1] == '.cgi' and self.is_cgi():
-                """Serve a POST request."""
                 self.run_cgi()
             else:
-                """Serve a GET request."""
                 f = http.server.SimpleHTTPRequestHandler.send_head(self)
                 if f:
                     try:
@@ -47,9 +43,8 @@ def wrap_handler():
 
 def main():
     if len(argv) != 3:
-        print('Enter port and directory to serve!')
+        print('ENTER PORT AND DIRECTORY')
         exit(1)
-
     port = argv[1]
     directory_name = argv[2]
     os.chdir(os.path.realpath(directory_name))
