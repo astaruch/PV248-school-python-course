bf54a9bb1c3bf76f24541554dfa2bc3e8a12303a
Andrej Staruch <staruch.andrej@gmail.com>
2018-12-03 21:40:38 +0100

    Concurrent requests testing

---
+++ 10-common-gateway-interface/multiple_requests.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+curl -X GET http://localhost:9001/sample-file.txt
+curl -X GET http://localhost:9001/
+curl -X GET http://localhost:9001/example.py.cgi
+curl -X GET http://localhost:9001/subdir
+curl -X GET http://localhost:9001/subdir/example.py.cgi
