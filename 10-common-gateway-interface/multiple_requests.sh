b6e0361fdb684713a183d37900e659bf5632a1d6
Andrej Staruch <staruch.andrej@gmail.com>
2018-12-03 21:55:40 +0100

    Non-blocking request - fixed

--- 10-common-gateway-interface/multiple_requests.sh
+++ 10-common-gateway-interface/multiple_requests.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-curl -X GET http://localhost:9001/long-request.py.cgi
-curl -X GET http://localhost:9001/sample-file.txt
-curl -X GET http://localhost:9001/
-curl -X GET http://localhost:9001/example.py.cgi
-curl -X GET http://localhost:9001/subdir
-curl -X GET http://localhost:9001/subdir/example.py.cgi
+curl -X GET http://localhost:9001/long-request.py.cgi &
+curl -X GET http://localhost:9001/sample-file.txt &
+curl -X GET http://localhost:9001/ &
+curl -X GET http://localhost:9001/example.py.cgi &
+curl -X GET http://localhost:9001/subdir &
+curl -X GET http://localhost:9001/subdir/example.py.cgi &
