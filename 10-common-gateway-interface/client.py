60f66c342a4f8a8578f516a25857613e48c196ed
Andrej Staruch <staruch.andrej@gmail.com>
2018-12-04 15:15:13 +0100

    More tests

--- 10-common-gateway-interface/client.py
+++ 10-common-gateway-interface/client.py
@@ -74,6 +74,27 @@ def main():
                                     params=querystring)
 
         print(response.text)
+    elif argv[1] == '6':
+        """POST hello_get.py.cgi with params. Expect:
+        <html>
+            <head>
+                <title>Hello - Second CGI Program</title>
+            </head>
+            <body>
+                <h2>Hello Andrej Staruch</h2>
+            </body>
+        </html>
+        """
+        url = "http://localhost:9001/hello_get.py.cgi"
+
+        payload = "------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"first_name\"\r\n\r\nAndrej\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"last_name\"\r\n\r\nStaruch\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--"
+        headers = {
+            'content-type': "multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW",
+            'cache-control': "no-cache",
+            'Postman-Token': "e4d979bf-7d0b-40d7-b1d2-457352d7d156"
+            }
+
+        response = requests.request("POST", url, data=payload, headers=headers)
 
 
 if __name__ == '__main__':
--- 10-common-gateway-interface/multiple_requests.sh
+++ 10-common-gateway-interface/multiple_requests.sh
@@ -6,3 +6,14 @@ curl -X GET http://localhost:9001/ &
 curl -X GET http://localhost:9001/example.py.cgi &
 curl -X GET http://localhost:9001/subdir &
 curl -X GET http://localhost:9001/subdir/example.py.cgi &
+curl -X POST \
+  http://localhost:9001/hello_get.py.cgi \
+  -H 'Postman-Token: 9c9245f4-3b95-4267-8221-1a5f02ec0511' \
+  -H 'cache-control: no-cache' \
+  -H 'content-type: multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW' \
+  -F first_name=Andrej \
+  -F last_name=Staruch &
+  curl -X GET \
+  'http://localhost:9001/hello_get.py.cgi?first_name=Andrej&last_name=Staruch' \
+  -H 'Postman-Token: c195e990-cdc1-4c85-adbd-33f71642b987' \
+  -H 'cache-control: no-cache' &
